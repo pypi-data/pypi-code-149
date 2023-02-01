a399bd11b59d18dc534156692976f2d624c16c41
Tom Forbes <tom@tomforb.es>
2023-02-01 16:11:59 +0000

    {"name":"bookerpubtool","version":"2023.2.2.0","file":"BookerPubTool-2023.2.2.0.tar.gz","path":"bookerpubtool/BookerPubTool-2023.2.2.0.tar.gz"}

---
+++ bookerpubtool/BookerPubTool-2023.2.2.0.tar.gz/BookerPubTool/ebook2site.py
@@ -0,0 +1,61 @@
+import re
+import os
+from os import path
+import shutil
+import sys
+import subprocess as subp
+import zipfile
+import jieba
+import xpinyin
+from io import BytesIO
+from .util import *
+
+def gen_proj_name(name):
+    name = re.sub(r'\.\w+$', '', name)
+    seg = re.findall(r'[\u4e00-\u9fff]+|[a-zA-Z0-9]+', name)
+    nseg = []
+    p = xpinyin.Pinyin()
+    for s in seg:
+        if re.search(r'[a-zA-Z0-9]', s):
+            nseg.append(s)
+        else:
+            subseg = jieba.cut(s)
+            for ss in subseg:
+                nseg.append(p.get_pinyin(ss).replace('-', ''))
+    res = '-'.join(nseg)
+    return res
+
+def ebook2site(args):
+    name = args.name
+    fname = args.file
+    dir = args.dir
+        
+    if not fname.endswith('.pdf') and \
+       not fname.endswith('.epub'):
+        print('请提供 PDF 或 EPUB')
+        return 
+    
+    if not name: name = gen_proj_name(path.basename(fname))
+    proj_dir = path.join(dir, name)
+    os.mkdir(proj_dir)
+    
+    if fname.endswith('.pdf'):
+        shutil.copy(fname, path.join(proj_dir, 'file.pdf'))
+        shutil.copy(
+            d('asset/pdf.html'), 
+            path.join(proj_dir, 'index.html'),
+        )
+    else:
+        shutil.copy(fname, path.join(proj_dir, 'file.epub'))
+        zip = zipfile.ZipFile(BytesIO(open(d('asset/epubjs-reader.zip'), 'rb').read()))
+        for f in zip.namelist(): zip.extract(f, proj_dir)
+        zip.close()
+    title = path.basename(fname).replace('.pdf', '').replace('.epub', '')
+    npm_name = npm_filter_name(name)
+    README_TMP = read_file(d('asset/readme.tmpl'), 'utf-8')
+    README_MD = README_TMP \
+        .replace('{title}', title) \
+        .replace('{docker_hub_name}', name) \
+        .replace('{pip_name}', name) \
+        .replace('{npm_name}', npm_name)
+    open(path.join(proj_dir, 'README.md'), 'w', encoding='utf8').write(README_MD)
---
+++ bookerpubtool/BookerPubTool-2023.2.2.0.tar.gz/BookerPubTool/npm.py
@@ -0,0 +1,96 @@
+import subprocess as subp
+from datetime import datetime
+import requests
+from pyquery import PyQuery as pq
+import tempfile
+import uuid
+import sys
+import shutil
+import os
+from os import path
+import re
+import stat
+import json
+from .util import *
+
+def config_npm(args):
+    subp.Popen(
+        ['npm', 'config', 'set', '//registry.npmjs.org/:_authToken', args.token],
+        shell=True,
+        stdout=subp.PIPE,
+        stderr=subp.PIPE
+    ).communicate()
+    
+def get_npm_new_version(name, curr=None):
+    now = datetime.now()
+    curr = curr or \
+        f'{now.year}.{now.month}{now.day:02d}'
+    r, err = subp.Popen(
+        ['npm', 'view', name, 'versions', '--json'],
+        stdout=subp.PIPE,
+        stderr=subp.PIPE,
+        shell=True
+    ).communicate()
+    if err: return 0
+    j = json.loads(r.decode('utf-8'))
+    j = [it for it in j if it.startswith(curr + '.')]
+    j = [int(it.split('.')[-1]) for it in j]
+    return 0 if len(j) == 0 else max(j) + 1
+    
+def publish_npm(args):
+    dir = path.abspath(args.dir)
+    # 检查目录
+    if not path.isdir(dir):
+        print('请提供目录')
+        return
+    files = os.listdir(dir)
+    if not 'index.html' in files or \
+       not 'README.md' in files:
+        print('请提供文档')
+        return
+    # 读取元信息
+    name = path.basename(dir)
+    pkg_name = npm_filter_name(name)
+    now = datetime.now()
+    ver = f'{now.year}.{now.month}{now.day:02d}.' + \
+          str(get_npm_new_version(name))
+    readme = read_file(path.join(dir, 'README.md'), 'utf-8')
+    desc = get_desc(readme)
+    print(f'name: {name}, pkg: {pkg_name}, ver: {ver}, desc: {desc}')
+    # 创建临时目录
+    pkg_dir = path.join(tempfile.gettempdir(), uuid.uuid4().hex)
+    os.mkdir(pkg_dir)
+    # 填充文档
+    shutil.copytree(dir, path.join(pkg_dir, 'doc'))
+    shutil.copy(
+        path.join(pkg_dir, 'doc/README.md'),
+        path.join(pkg_dir, 'README.md')
+    )
+    # 主程序
+    shutil.copy(
+        d('asset/server.js'),
+        path.join(pkg_dir, 'index.js')
+    )
+    # 配置
+    pkg_json = json.loads(read_file(d('asset/package.tmpl'), 'utf-8'))
+    pkg_json['name'] = pkg_name
+    pkg_json['version'] = ver
+    pkg_json['description'] = desc
+    pkg_json['repository']['url'] = \
+        f'git+https://github.com/apachecn/{name}.git'
+    pkg_json['bin'] = {}
+    pkg_json['bin'][pkg_name] = "index.js"
+    pkg_json['bin'][name] = "index.js"
+    write_file(
+        path.join(pkg_dir, 'package.json'),
+        json.dumps(pkg_json), 'utf-8'
+    )
+    # 发布
+    os.chdir(pkg_dir)
+    subp.Popen(
+        ['npm', 'publish'],
+        shell=True
+    ).communicate()
+    # 删除临时目录
+    os.chdir('..')
+    rmtree(pkg_dir)
