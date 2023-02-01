703cfa58e2a9baca748ead6d82c7d9d86ae878af
Tom Forbes <tom@tomforb.es>
2023-02-01 13:50:18 +0000

    {"name":"ibooker-witchcraft-zh","version":"2023.2.1.0","file":"ibooker_witchcraft_zh-2023.2.1.0-py3-none-any.whl","path":"ibooker-witchcraft-zh/ibooker_witchcraft_zh-2023.2.1.0-py3-none-any.whl"}

---
+++ ibooker-witchcraft-zh/ibooker_witchcraft_zh-2023.2.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ibooker-witchcraft-zh
+https://github.com/apachecn/ibooker-witchcraft-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.2.1.0"
