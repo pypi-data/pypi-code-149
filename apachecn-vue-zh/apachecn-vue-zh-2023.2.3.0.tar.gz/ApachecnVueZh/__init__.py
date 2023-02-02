5c6cdbe14953c02af22ef6aee56711e9613bb5d6
Tom Forbes <tom@tomforb.es>
2023-02-02 17:13:49 +0000

    {"name":"apachecn-vue-zh","version":"2023.2.3.0","file":"apachecn-vue-zh-2023.2.3.0.tar.gz","path":"apachecn-vue-zh/apachecn-vue-zh-2023.2.3.0.tar.gz"}

---
+++ apachecn-vue-zh/apachecn-vue-zh-2023.2.3.0.tar.gz/ApachecnVueZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-vue-zh
+https://github.com/apachecn/apachecn-vue-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.2.3.0"
