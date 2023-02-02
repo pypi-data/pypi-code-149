7d51f1c36c2a6e020cd23e91e0c0980493fbfede
Tom Forbes <tom@tomforb.es>
2023-02-02 05:35:29 +0000

    {"name":"langji-course-notes","version":"2023.2.2.0","file":"langji-course-notes-2023.2.2.0.tar.gz","path":"langji-course-notes/langji-course-notes-2023.2.2.0.tar.gz"}

---
+++ langji-course-notes/langji-course-notes-2023.2.2.0.tar.gz/LangjiCourseNotes/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""langji-course-notes
+https://github.com/apachecn/langji-course-notes"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.2.2.0"
